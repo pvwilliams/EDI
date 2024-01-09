d6c4b23f6fffc56930b6aebff12054449b3ca3ec
pvwilliams <144709010+pvwilliams@users.noreply.github.com>
2024-01-09 11:21:41 -0500

    update

---
+++ CreativeCoding/2x2loop/sketch.js
@@ -0,0 +1,27 @@
+function setup() {//runs once
+  createCanvas(720, 720);
+  fill('red');
+  strokeWeight (8);
+}
+
+function draw() {//runs in a loop
+  background(220);
+  
+  translate (60,60);
+  
+    for(var y = 0; y < 600; y = y + 300) 
+   {
+      
+      
+  for(var x = 0; x < 600; x = x + 300)
+    {  
+      quad(
+      x,y,
+      x + 300,y,
+      x + 300,y + 300,
+      x,y + 300
+           );//loop to create a row in the x direction
+    }
+   }
+
+}
