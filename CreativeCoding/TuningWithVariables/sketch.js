086c32847c4daaeec5a995f04a7f7880c934f817
pvwilliams <144709010+pvwilliams@users.noreply.github.com>
2024-01-09 11:43:33 -0500

    update

--- CreativeCoding/TuningWithVariables/sketch.js
+++ CreativeCoding/TuningWithVariables/sketch.js
@@ -1,7 +1,30 @@
-function setup() {
-  
+function setup() {//runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('red');
+  strokeWeight (1);
 }
 
-function draw() {
+function draw() {//runs in a loop
+  
+  var num = 100;
+  var sideLen = windowWidth/num;
+  
+    for(var y = 0; y < windowHeight; y = y + sideLen) 
+   {//loop to create a grid the y direction
+      
+      
+  for(var x = 0; x < windowWidth; x = x + sideLen)
+    {  
+      quad(
+      x,y,
+      x + sideLen,y,
+      x + sideLen,y + sideLen,
+      x,y + sideLen
+           );//loop to create a row in the x direction
+    }
+   }
+}
 
+function windowResized(){
+  resizeCanvas (windowWidth, windowHeight)
 }
