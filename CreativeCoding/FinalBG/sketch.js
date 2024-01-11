0d5ff71c29970eac5cb19b9076f6ad97dcf09b4f
pvwilliams <144709010+pvwilliams@users.noreply.github.com>
2024-01-11 09:56:21 -0500

    update

---
+++ CreativeCoding/FinalBG/sketch.js
@@ -0,0 +1,31 @@
+let=gif;
+
+function preload (){
+gif = loadImage ('Assests/BeeGifBG.gif');
+}
+
+function setup() {//runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('red');
+  strokeWeight (1);
+}
+
+function draw() {//runs in a loop
+  
+  var num = 100;
+  var sideLen = windowWidth/num;
+  
+    for(var y = 0; y < windowHeight; y = y + sideLen) 
+   {//loop to create a grid the y direction
+      
+      
+  for(var x = 0; x < windowWidth; x = x + sideLen)
+    {  
+    image (gif, x, y, sideLen, sideLen);
+    }
+   }
+}
+
+function windowResized(){
+  resizeCanvas (windowWidth, windowHeight)
+}
